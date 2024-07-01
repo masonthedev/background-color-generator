b2c2208faecf3d8994ee3256a150aaa7c2dfef13
Mason Franceschi <142521332+masonthedev@users.noreply.github.com>
2024-07-01 16:56:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,43 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-var button = document.querySelector("random");
+var button = document.getElementById("random");
+var arrow = document.getElementById("arrow");
+var angle = 0;
 
-function randomize () {
+function randomize (color1, color2) {
 	var randomColor1 = "#" + Math.floor(Math.random()*16777215).toString(16);
-    var randomColor2 = "#" + Math.floor(Math.random()*16777215).toString(16);
+	var randomColor2 = "#" + Math.floor(Math.random()*16777215).toString(16);
 
 	var color1 = document.querySelector(".color1");
-    var color2 = document.querySelector(".color2");
-    
-    color1.value = randomColor1;
-    color2.value = randomColor2;
-    
-    setGradient();
+	var color2 = document.querySelector(".color2");
+
+	color1.value = randomColor1;
+	color2.value = randomColor2;
+
+	setGradient();
 }
 
-function setGradient(){
-	body.style.background = 
-	"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+function setGradient() {
+	body.style.background = "linear-gradient(" + angle + "deg, " + color1.value + ", " + color2.value + ")";
 	css.textContent = body.style.background + ";";
 }
-color1.addEventListener("input",setGradient);
-color2.addEventListener("input",setGradient);
+
+function rotateArrow(degrees) {
+	arrow.style.transform = "rotate(" + degrees + "deg)";
+	angle = degrees;
+}
+
+function rotateArrowAndSetGradient() {
+	rotateArrow(angle + 22.5);
+	setGradient();
+}
+
+button.addEventListener("click", randomize);
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+arrow.addEventListener("click", rotateArrowAndSetGradient);
+
+// set default gradient on page load
+setGradient();
+
